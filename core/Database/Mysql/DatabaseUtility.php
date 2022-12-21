d7571d35a6760103c83bd56bd7c98b6e3f209c45
Antônio Paulo <apaulo2007@gmail.com>
2022-12-21 20:17:52 -0300

    Update DatabaseUtility.php
    Added validation of $query and $prefix to prevent SQL injection attacks
    Used mysql_real_escape_string to escape special characters in the checkSQL function
    
    DESCRIPTION:
    The original code did not have any measures in place to prevent SQL injection attacks. To address this, I added validation of the $query and $prefix parameters in the prefixQuery function using the checkSQL function. The checkSQL function uses mysql_real_escape_string to escape special characters in the input string, and then checks if the escaped string is different from the original string. If it is different, it means that special characters were found and escaped, and therefore the input string is not safe. 
    
    Observation: 
    I am participating in the HackerOne program and have also uploaded my findings there. I hope my contributions will be of assistance in improving security measures.

--- core/Database/Mysql/DatabaseUtility.php
+++ core/Database/Mysql/DatabaseUtility.php
@@ -4,12 +4,9 @@ namespace ImpressCMS\Core\Database\Mysql;
 use ImpressCMS\Core\Database\DatabaseUtilityInterface;
 
 /**
- * Redefinida por Claudia A. V. Callegari
- * Participação de Rodrigo Lima
- *
  * Redefined by Claudia A. V. Callegari
  * Participation Rodrigo Lima
- *
+ * Participation Paulinho Neto 
  * ImpressCMS.org
  *
  * @copyright	The ImpressCMS Project - http://www.impresscms.org/
@@ -17,64 +14,99 @@ use ImpressCMS\Core\Database\DatabaseUtilityInterface;
  */
 abstract class DatabaseUtility implements DatabaseUtilityInterface {
 
-	/**
-	 * add a prefix.'_' to all tablenames in a query
-	 *
-	 * @param   string  $query  valid SQL query string
-	 * @param   string  $prefix prefix to add to all table names
-	 * @return  mixed   FALSE on failure
-	 */
-	public static function prefixQuery($query, $prefix) {
-		$pattern = "/^(INSERT INTO|CREATE TABLE|ALTER TABLE|UPDATE)(\s)+([`]?)([^`\s]+)\\3(\s)+/siU";
-		$pattern2 = "/^(DROP TABLE)(\s)+([`]?)([^`\s]+)\\3(\s)?$/siU";
+    /**
+     * Add a prefix.'_' to all tablenames in a query
+     *
+     * @param string $query  valid SQL query string
+     * @param string $prefix prefix to add to all table names
+     * @return mixed FALSE on failure
+     */
+    public static function prefixQuery($query, $prefix) {
+        $pattern = "/^(INSERT INTO|CREATE TABLE|ALTER TABLE|UPDATE)(\s)+([`]?)([^`\s]+)\\3(\s)+/siU";
+        $pattern2 = "/^(DROP TABLE)(\s)+([`]?)([^`\s]+)\\3(\s)?$/siU";
 
-		if (preg_match($pattern, $query, $matches)) {
-			$replace = "\\1 " . $prefix . "_\\4\\5";
-			$matches [0] = preg_replace($pattern, $replace, $query);
-			$query = $matches [0];
-			if (preg_match('/REFERENCES/', $query) or preg_match('/DROP FOREIGN KEY/', $query)) {
+        // Validate $query and $prefix to prevent SQL injection attacks
+        if (!self::checkSQL($query) || !self::checkSQL($prefix)) {
+            return false;
+        }
 
-				$matches_1 = $matches; // claudia
-				//$pattern = "/(REFERENCES)(\s)+([`]?)([^`\s]+)\\3(\s)+/siU";
-				// alterado abaixo 03/10/2011, sendo que funcionou para ADD CONSTRAINT e não para DROP FOREIGN KEY
-				$pattern = "/(REFERENCES|DROP FOREIGN KEY|ADD CONSTRAINT)(\s)+([`]?)([^`\s]+)\\3(\s)+/siU";
-				if (preg_match($pattern, $query, $matches)) {
-					$matches [0] = preg_replace($pattern, $replace, $query);
-					$matches_1[0] = $matches[0]; // claudia
-					$matches = $matches_1; // claudia
-				}
-			}
-			return $matches;
-		} elseif (preg_match($pattern2, $query, $matches)) {
-			$replace = "\\1 " . $prefix . "_\\4\\5";
-			$matches [0] = preg_replace($pattern2, $replace, $query);
+        if (preg_match($pattern, $query, $matches)) {
+            $replace = "\\1 " . $prefix . "_\\4\\5";
+            $matches[0] = preg_replace($pattern, $replace, $query);
+            $query = $matches[0];
+            if (preg_match('/REFERENCES/', $query) || preg_match('/DROP FOREIGN KEY/', $query)) {
+                $matches_1 = $matches; // claudia
+                // $pattern = "/(REFERENCES)(\s)+([`]?)([^`\s]+)\\3(\s)+/siU";
+                // alterado abaixo 03/10/2011, sendo que funcionou para ADD CONSTRAINT e não para DROP FOREIGN KEY
+                $pattern = "/(REFERENCES|DROP FOREIGN KEY|ADD CONSTRAINT)(\s)+([`]?)([^`\s]+)\\3(\s)+/siU";
+                if (preg_match($pattern, $query, $matches)) {
+                    $matches[0] = preg_replace($pattern, $replace, $query);
+                    $matches_1[0] = $matches[0]; // claudia
+                    $matches = $matches_1;
+    /**
+     * Removes comment and splits large sql files into individual queries
+     * Function from phpMyAdmin (http://phpwizard.net/projects/phpMyAdmin/)
+     *
+     * Last revision: September 23, 2001 - gandon
+     *
+     * @param array $ret   the split sql commands
+     * @param string $sql the sql commands
+     * @return bool always true
+     */
+    public static function splitSqlFile(&$ret, $sql) {
+        $sql = trim($sql);
+        $sql_len = strlen($sql);
+        $char = '';
+        $string_start = '';
+        $in_string = false;
+        $time0 = time();
 
-			return $matches;
-		} else {
+        for ($i = 0; $i < $sql_len; ++$i) {
+            $char = $sql[$i];
 
-			return false;
-		}
-	}
+            // We are in a string, check for not escaped end of strings except for
+            // backquotes that can't be escaped
+            if ($in_string) {
+                for (;;) {
+                    $i = strpos($sql, $string_start, $i);
+                    // No end of string found -> add the current substring to the
+                    // returned array
+                    if (!$i) {
+                        $ret[] = $sql;
+                        return true;
+                    }
+                    // Backquotes or no backslashes before quotes: it's indeed the
+                    // end of the string -> exit the loop
+                    else if ($string_start == '`' || $sql[$i - 1] != '\\') {
+                        $string_start = '';
+                        $in_string = false;
+                        break;
+                    }
+                    // one or more Backslashes before the presumed end of string...
+                    else {
+                        // ... first checks for escaped backslashes
+                        $j = 2;
+                        $escaped_backslash = false;
+                        while ($i - $j > 0 && $sql[$i - $j] == '\\') {
+                            $escaped_backslash = !$escaped_backslash;
+    /**
+     * Determine if the SQL string is safe
+     *
+     * @param string $sql
+     * @return bool TRUE if the string is safe
+     */
+    public static function checkSQL($sql) {
+        // Use mysql_real_escape_string to escape special characters
+        $safe_sql = mysql_real_escape_string($sql);
 
-	/**
-	 * Removes comment and splits large sql files into individual queries
-	 * Function from phpMyAdmin (http://phpwizard.net/projects/phpMyAdmin/)
-	 *
-	 * Last revision: September 23, 2001 - gandon
-	 *
-	 * @param   array    the split sql commands
-	 * @param   string   the sql commands
-	 * @return  boolean  always true
-	 */
-	public static function splitSqlFile(&$ret, $sql) {}
+        // Check if $safe_sql is equal to $sql. If not, it means that
+        // mysql_real_escape_string found and escaped special characters, so $sql
+        // is not safe.
+        if ($safe_sql != $sql) {
+            return false;
+        }
 
-
-	/**
-	 * Determine if the SQL string is safe
-	 *
-	 * @param string $sql
-	 * @return bool	TRUE if the string is safe
-	 */
-	public static function checkSQL($sql) {}
-
+        // $sql is safe
+        return true;
+    }
 }
